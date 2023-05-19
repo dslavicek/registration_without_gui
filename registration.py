807095ee07b6bc08c0fd2fefeb4ccd0a403f7fdd
dslavicek <slavicek.david29@gmail.com>
2023-05-19 18:43:15 +0200

    improved affine registration

--- registration.py
+++ registration.py
@@ -76,7 +76,7 @@ def rigid_registration(ref, sample, max_iters=120, mu=0.02, datatype=torch.float
     return output_dict
 
 
-def affine_registration(ref, sample, max_iters=120, mu=0.02, datatype=torch.float32, verbose=False):
+def affine_registration(ref, sample, max_iters=120, mu=0.02, datatype=torch.float32, verbose=False, used_device='cpu'):
     # this function performs registration of two 4D pytorch tensors
     # inputs - reference tensor, tensor of moving images, max. number of iterations, size of registration step,
     # datatype, verbose mode
@@ -95,37 +95,43 @@ def affine_registration(ref, sample, max_iters=120, mu=0.02, datatype=torch.floa
     width = ref.shape[3]
     registered_tens = None
 
-    mask = torch.ones(sample.shape, dtype=datatype)
+    ref = ref.to(used_device)
+    sample = sample.to(used_device)
 
-    x_shift = torch.zeros(batch_size, dtype=datatype, requires_grad=True)
-    y_shift = torch.zeros(batch_size, dtype=datatype, requires_grad=True)
-    angle_rad = torch.zeros(batch_size, dtype=datatype, requires_grad=True)
+    mask = torch.ones(sample.shape, dtype=datatype, device=used_device)
 
-    x_scale = torch.ones(batch_size, dtype=datatype, requires_grad=True)
-    y_scale = torch.ones(batch_size, dtype=datatype, requires_grad=True)
+    x_shift = torch.zeros(batch_size, dtype=datatype, requires_grad=True, device=used_device)
+    y_shift = torch.zeros(batch_size, dtype=datatype, requires_grad=True, device=used_device)
+    angle_rad = torch.zeros(batch_size, dtype=datatype, requires_grad=True, device=used_device)
 
-    shear = torch.zeros(batch_size, dtype=datatype, requires_grad=True)
+    x_scale = torch.ones(batch_size, dtype=datatype, requires_grad=True, device=used_device)
+    y_scale = torch.ones(batch_size, dtype=datatype, requires_grad=True, device=used_device)
 
-    optimizer = torch.optim.Adam([x_shift, y_shift, angle_rad, x_scale, y_scale, shear], lr=mu)
+    shear_x = torch.zeros(batch_size, dtype=datatype, requires_grad=True, device=used_device)
+    shear_y = torch.zeros(batch_size, dtype=datatype, requires_grad=True, device=used_device)
+
+    optimizer = torch.optim.Adam([x_shift, y_shift, angle_rad, x_scale, y_scale, shear_x, shear_y], lr=mu)
     losses = []
     for i in range(max_iters):
         if verbose and (i == 0 or i % 5 == 4 or i == max_iters - 1):
             print("iteration", i + 1, " out of ", max_iters)
         # prepare transformation matrix
-        T = translation_mat(x_shift, y_shift, datatype)
-        R = rotation_mat(angle_rad, datatype)
-        S = scale_mat(x_scale, y_scale, datatype)
-        SH = shear_mat(shear, datatype)
+        T = translation_mat(x_shift, y_shift, datatype, used_device)
+        R = rotation_mat(angle_rad, datatype, used_device)
+        S = scale_mat(x_scale, y_scale, datatype, used_device)
+        SH = shear_mat(shear_x, shear_y, datatype, used_device)
         t_mat = T @ R @ S @ SH
         t_mat = t_mat[:, 0:2, :]
 
 
         # transform image
-        grid = F.affine_grid(t_mat, sample.shape)
-        registered_tens = F.grid_sample(sample, grid, padding_mode="zeros")
+        grid = F.affine_grid(t_mat, sample.shape, align_corners=False)
+        registered_tens = F.grid_sample(sample, grid, padding_mode="zeros",
+                                        align_corners=False)
         # transform mask
         with torch.no_grad():
-            mask = F.grid_sample(mask, grid, padding_mode="zeros")
+            mask = F.grid_sample(mask, grid, padding_mode="zeros",
+                                 align_corners=False)
         # calculate loss function
         diff = (registered_tens - ref) ** 2
         diff = diff * mask
@@ -146,38 +152,36 @@ def affine_registration(ref, sample, max_iters=120, mu=0.02, datatype=torch.floa
         "angles_rad": angle_rad.detach().cpu().numpy(),
         "losses": losses,
         "registered_tens": registered_tens.detach().cpu(),
-        "transformation_matrices": t_mat.detach()
+        "transformation_matrices": t_mat.detach().cpu()
     }
     return output_dict
 
 
-def translation_mat(x_shift, y_shift, datatype):
-    t_mat = torch.tensor([[[1, 0, 0], [0, 1, 0], [0, 0, 1]]], dtype=datatype)
-    t_mat = t_mat.repeat((len(x_shift), 1, 1))
+def translation_mat(x_shift, y_shift, datatype, used_device):
+    t_mat = torch.eye(3, dtype=datatype, device=used_device).repeat((len(x_shift), 1, 1))
     t_mat[:, 0, 2] = x_shift
     t_mat[:, 1, 2] = y_shift
     return t_mat
 
 
-def scale_mat(scale_x, scale_y, datatype):
-    t_mat = torch.tensor([[[1, 0, 0], [0, 1, 0], [0, 0, 1]]], dtype=datatype)
-    t_mat = t_mat.repeat((len(scale_x), 1, 1))
+def scale_mat(scale_x, scale_y, datatype, used_device):
+    t_mat = torch.eye(3, dtype=datatype, device=used_device).repeat((len(scale_x), 1, 1))
     t_mat[:, 0, 0] = scale_x
     t_mat[:, 1, 1] = scale_y
     return t_mat
 
 
-def rotation_mat(angle_rad, datatype):
-    t_mat = torch.tensor([[[1, 0, 0], [0, 1, 0], [0, 0, 1]]], dtype=datatype)
-    t_mat = t_mat.repeat((len(angle_rad), 1, 1))
+def rotation_mat(angle_rad, datatype, used_device):
+    t_mat = torch.eye(3, dtype=datatype, device=used_device).repeat((len(angle_rad), 1, 1))
     t_mat[:, 0, 0] = torch.cos(angle_rad)
     t_mat[:, 0, 1] = -torch.sin(angle_rad)
     t_mat[:, 1, 0] = torch.sin(angle_rad)
     t_mat[:, 1, 1] = torch.cos(angle_rad)
     return t_mat
 
-def shear_mat(shear, datatype):
-    t_mat = torch.tensor([[[1, 0, 0], [0, 1, 0], [0, 0, 1]]], dtype=datatype)
-    t_mat = t_mat.repeat((len(shear), 1, 1))
-    t_mat[:, 0, 1] = shear
+
+def shear_mat(shear_x, shear_y, datatype, used_device):
+    t_mat = torch.eye(3, dtype=datatype, device=used_device).repeat((len(shear_x), 1, 1))
+    t_mat[:, 0, 1] = shear_x
+    t_mat[:, 1, 0] = shear_y
     return t_mat
